8e49d81dd518fbda2fb76e4f0a9a088c4e7f5ea2
Vagner Junior <vagnerflorindojunior@gmail.com>
2022-12-01 00:17:49 -0300

    Ajuste no JS para uma nova função quando clicar no button

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,25 +1,46 @@
-const sidebar = document.querySelector('.sidebar');
-const containerTop = document.querySelector('.container-top'); 
-const logo = document.querySelector('.logo')
-const list = document.querySelector('li'); 
-const appearParagraph = document.querySelectorAll('.sidebar-menu-icons-name')
 
-const appearInput = document.querySelector('input'); 
-const containerBottom = document.querySelector('.container-bottom'); 
-const containerBottomPerson = document.querySelector('.container-bottom-person'); 
+let containerTop = document.querySelector('.container-top'); 
+let logo = document.querySelector('.logo')
+let list = document.querySelector('li'); 
+let appearParagraph = document.querySelectorAll('.sidebar-menu-icons-name')
+let appearInput = document.querySelector('input'); 
+let containerBottom = document.querySelector('.container-bottom'); 
+let containerBottomPerson = document.querySelector('.container-bottom-person'); 
 
-console.log(appearParagraph)
+let sidebar = document.querySelector('.sidebar');
+    sidebar = true;
 
 function appearSidebar() {
-    sidebar.setAttribute('id', 'opened-sidebar');
-    containerTop.setAttribute('id', 'container-top-center')
-    logo.setAttribute('id','logo')
-    list.setAttribute('id','list')
-    appearInput.setAttribute('id','list-text-appear')
-    // quando tiver uma lista usar o forEach 
-    appearParagraph.forEach(e => {
-        e.setAttribute('id', 'list-text-appear')
-    })
-    containerBottom.setAttribute('id','container-bottom-center')
-    containerBottomPerson.setAttribute('id','container-bottom-person-appear')
+    sidebar = !sidebar
+
+    if (sidebar) {
+        document.querySelector('.sidebar').style.width = "250px";
+        document.querySelector('main').style.left = "250px"
+        
+        containerTop.setAttribute('id', 'container-top-center')
+        logo.setAttribute('id','logo')
+        list.setAttribute('id','list')
+        appearInput.setAttribute('id','list-text-appear')
+        // quando tiver uma lista usar o forEach 
+        appearParagraph.forEach(e => {
+            e.setAttribute('id', 'list-text-appear')
+        })
+        containerBottom.setAttribute('id','container-bottom-center')
+        containerBottomPerson.setAttribute('id','container-bottom-person-appear')
+    } else {
+        document.querySelector('.sidebar').style.width = "70px";
+        document.querySelector('main').style.left = "70px"
+
+
+        containerTop.removeAttribute('id', 'container-top-center')
+        logo.removeAttribute('id','logo')
+        list.removeAttribute('id','list')
+        appearInput.removeAttribute('id','list-text-appear')
+        // quando tiver uma lista usar o forEach 
+        appearParagraph.forEach(e => {
+            e.removeAttribute('id', 'list-text-appear')
+        })
+        containerBottom.removeAttribute('id','container-bottom-center')
+        containerBottomPerson.removeAttribute('id','container-bottom-person-appear')
+    }
 }
